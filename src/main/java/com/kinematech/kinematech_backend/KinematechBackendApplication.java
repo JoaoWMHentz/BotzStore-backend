851b1a6b2df8c5059ef6ec65edb40bfe59288de6
João Witor Müller Hentz <joaowitormiiller@gmail.com>
2025-05-12 08:46:43 -0300

    Initialize Kinematech Backend project with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/kinematech/kinematech_backend/KinematechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kinematech.kinematech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinematechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinematechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinematech/kinematech_backend/KinematechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinematech.kinematech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinematechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
